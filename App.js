2f59ebba1a187f1bb97dd699366c676f1d530a75
Muhammed İşiyok <isiyokm1993@gmail.com>
2024-01-03 21:20:02 +0300

    finished section

--- App.js
+++ App.js
@@ -1,10 +1,138 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Alert, Platform } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
+
+//// START: NEWLY ADDED FUNCTIONS ////
+// const allowsNotificationsAsync = async () => {
+//   const settings = await Notifications.getPermissionsAsync();
+//   return (
+//     settings.granted ||
+//     settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+//   );
+// };
+
+// const requestPermissionsAsync = async () => {
+//   return await Notifications.requestPermissionsAsync({
+//     ios: {
+//       allowAlert: true,
+//       allowBadge: true,
+//       allowSound: true,
+//       allowAnnouncements: true,
+//     },
+//   });
+// };
+//// END: NEWLY ADDED FUNCTIONS ////
 
 export default function App() {
+  useEffect(() => {
+    async function configurePushNotifications() {
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+      if (finalStatus !== "granted") {
+        const { status } = await requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        Alert.alert(
+          "Permission required",
+          "Push notifications need the appropriate permissions."
+        );
+        return;
+      }
+
+      const pushTokenData = await Notifications.getExpoPushTokenAsync({projectId: "b9139eeb-b940-48ea-9e33-d3ffe9ddc205"});
+      console.log("PUSH TOKEN: ", pushTokenData);
+
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.DEFAULT,
+        });
+      }
+    }
+
+    configurePushNotifications();
+
+  }, []);
+
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("NOTIFICATION RECEIVED: ", notification);
+        const userName = notification.request.content.data.userName;
+        console.log("USER NAME: ", userName);
+      }
+    );
+
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log("NOTIFICATION RESPONSE RECEIVED");
+        console.log(response);
+      }
+    );
+
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
+  async function scheduleNotificationHandler() {
+    //// START: CALL FUNCTIONS HERE ////
+    // const hasPushNotificationPermissionGranted =
+    //   await allowsNotificationsAsync();
+
+    // if (!hasPushNotificationPermissionGranted) {
+    //   await requestPermissionsAsync();
+    // }
+    //// END: CALL FUNCTIONS HERE ////
+
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first local notification",
+        body: "This is the body of the notification",
+        data: { userName: "Max" },
+      },
+      trigger: { seconds: 5 },
+    });
+  }
+
+  function sendPushNotificationHandler() {
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        to: 'ExponentPushToken[8CM0lRC-aB_l_omLiR8HpB]',
+        title: 'Test - sent from a device!',
+        body: 'This is a test'
+      })
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
+      <Button
+        title="Send Push Notification"
+        onPress={sendPushNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +141,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
